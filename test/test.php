fe927c8468ff01bc5d02602c72dc501b3ad166db
Rasmus Schultz <rasmus@mindplay.dk>
2017-01-26 14:33:14 +0100

    remove dead imports

--- test/test.php
+++ test/test.php
@@ -1,34 +1,5 @@
 <?php
 
-use mindplay\sql\exceptions\SQLException;
-use mindplay\sql\exceptions\TransactionAbortedException;
-use mindplay\sql\framework\BufferedPSRLogger;
-use mindplay\sql\framework\MapperProvider;
-use mindplay\sql\framework\mappers\BatchMapper;
-use mindplay\sql\framework\mappers\RecordMapper;
-use mindplay\sql\framework\pdo\PDOProvider;
-use mindplay\sql\framework\pdo\PreparedPDOStatement;
-use mindplay\sql\framework\PreparedStatement;
-use mindplay\sql\framework\QueryFormatter;
-use mindplay\sql\framework\Result;
-use mindplay\sql\framework\Statement;
-use mindplay\sql\model\DatabaseContainerFactory;
-use mindplay\sql\model\expr;
-use mindplay\sql\model\query\Query;
-use mindplay\sql\model\schema\Column;
-use mindplay\sql\model\schema\Type;
-use mindplay\sql\model\types\BoolType;
-use mindplay\sql\model\types\IntType;
-use mindplay\sql\model\types\JSONType;
-use mindplay\sql\model\types\StringType;
-use mindplay\sql\model\types\TimestampType;
-use mindplay\sql\mysql\MySQLConnection;
-use mindplay\sql\mysql\MySQLDatabase;
-use mindplay\sql\postgres\PostgresConnection;
-use mindplay\sql\postgres\PostgresDatabase;
-use Mockery\MockInterface;
-use Psr\Log\LogLevel;
-
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 require __DIR__ . '/fixtures.php';
